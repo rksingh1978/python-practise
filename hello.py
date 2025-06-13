dd31d4177f406d97f6781a369f561df17ffdae3f
Rakesh Kumar Singh <rksinusa@gmail.com>
2025-06-13 19:26:51 +0530

    how to comment in python

--- hello.py
+++ hello.py
@@ -2,7 +2,16 @@
 print("Hi")
 
 # print if statement
-if 3 > 2
+if 3 > 2:
     print("3 is greater than 2")
 
-#
+# print if statement with error due to below error:
+# IndentationError: expected an indented block, try removing the indentation on print stmt to throw error
+if 2 == 2:
+    print("2 is equal to 2")
+
+"""
+multi line comment can be written like this
+"""
+print("multi line comment above using string literal")
+
