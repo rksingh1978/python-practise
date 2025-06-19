8ce94fd8ec8de13abe4cb5a1efba4571e56625df
Rakesh Kumar Singh <rksinusa@gmail.com>
2025-06-20 02:17:01 +0530

    typecast and random

---
+++ pythonDatatypes.py
@@ -0,0 +1,44 @@
+# datatyes in Python
+
+#int
+x = 3
+print(x)
+print(type(x))
+
+#float
+y = 5.3
+print (y)
+print(type(y))
+
+#complex - 2 ways to define
+z = 1j
+# print(z),
+# or
+z = complex(1j) #complex here becomes constructor
+print(z)
+print(type(z))
+
+# typecast int, float, complex
+x = float(x)
+y = int(y)
+z = complex(z)
+print(x)
+print(y)
+print(z)
+
+#cast = str
+print(int(3.0))
+print(float(4))
+print(str(3.5))
+print(str("hi"))
+
+#random function - inbuilt lib
+import random
+xrand = random.randrange(1, 10)
+print(xrand)
+
+# 
+
+
+
+
